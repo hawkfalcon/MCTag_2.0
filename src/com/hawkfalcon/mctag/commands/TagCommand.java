98eb010161aaef60f17e5e0e86249f813cca691a
gomeow <davismariotti@gmail.com>
2013-02-06 08:01:08 -0800

    Add a constructor

--- src/com/hawkfalcon/mctag/commands/TagCommand.java
+++ src/com/hawkfalcon/mctag/commands/TagCommand.java
@@ -4,8 +4,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.hawkfalcon.mctag.MCTag;
+
 public class TagCommand implements CommandExecutor {
 
+	MCTag plugin;
+
+	public TagCommand(MCTag mct) {
+		this.plugin = mct;
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("tag")||cmd.getName().equalsIgnoreCase("mctag")) {
