ed1f4b7e1449a01fc5781a6d7333776da4180a0a
gomeow <davismariotti@gmail.com>
2013-02-05 21:02:04 -0800

    Add arenas.yml support

--- src/com/hawkfalcon/mctag/util/TagUtil.java
+++ src/com/hawkfalcon/mctag/util/TagUtil.java
@@ -1,5 +1,50 @@
 package com.hawkfalcon.mctag.util;
 
-public class TagUtil {
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.hawkfalcon.mctag.MCTag;
+
+public class TagUtil {
+	
+	public  MCTag plugin;
+	public  File arenasFile;
+	public  FileConfiguration arenas;
+	
+	public TagUtil(MCTag plugin) {
+		this.plugin = plugin;
+	}
+	
+	
+	public  void reloadArenas() {
+	    if (arenasFile == null) {
+	    	arenasFile = new File(plugin.getDataFolder(), "arenas.yml");
+	    }
+	    arenas = YamlConfiguration.loadConfiguration(arenasFile);
+	}
+
+	
+	public  FileConfiguration getArenas() {
+	    if (arenas == null) {
+	        this.reloadArenas();
+	    }
+	    return arenas;
+	}
+	
+	public  void saveArenas() {
+	    if (arenas == null || arenas == null) {
+	    return;
+	    }
+	    try {
+	    	getArenas().save(arenasFile);
+	    } catch (IOException ex) {
+	        plugin.getLogger().log(Level.SEVERE, "Could not save config to " + arenas, ex);
+	    }
+	}
+	
+	
 }
