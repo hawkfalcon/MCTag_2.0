00ee6dc9c9570aea4ee85fd0f675f8b8e9cb58a0
hawkfalcon <hawkfalcon1@gmail.com>
2013-02-06 18:35:23 -0800

    Interaction event cancelling ingame

--- src/com/hawkfalcon/mctag/MCTag.java
+++ src/com/hawkfalcon/mctag/MCTag.java
@@ -1,5 +1,6 @@
 package com.hawkfalcon.mctag;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.bukkit.event.Listener;
@@ -16,8 +17,8 @@ public class MCTag extends JavaPlugin {
 	Listener listener;
 
 	String commands = "Commands: \n /tag <join|leave> - join or leave the game \n /tag <start|stop> - start and stop game \n /tag it - view tagged player \n /tag players - view joined playrs \n /tag tagback <allow|forbid> - allow and forbid tagback \n /tag freezetag <on|off> - turn freeze tag on and off \n /tag reload - reloads the config \n /tag setspawn - set arena spawnpoint";
-
-
+	//placeholder, replaced with hashmap used to store playing players
+	public ArrayList playersPlaying;
 	public static TagUtil util;
 	public static GlobalVariables vars;
 	public static PluginManager pm;
--- src/com/hawkfalcon/mctag/listeners/EventListener.java
+++ src/com/hawkfalcon/mctag/listeners/EventListener.java
@@ -1,7 +1,46 @@
 package com.hawkfalcon.mctag.listeners;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+import com.hawkfalcon.mctag.MCTag;
 
 public class EventListener implements Listener {
+		public  MCTag plugin;
+		public EventListener(MCTag plugin) {
+			this.plugin = plugin;
+		}
+		
+	    //stop placing while ingame
+	 public void onBlockPlace(BlockPlaceEvent event) {
+	        String player = event.getPlayer().getName();
+	        if (plugin.playersPlaying.contains(player)) {
+	            event.setCancelled(true);
+	        }
+	    }
+
+	    //stop breaking while ingame
+	    @EventHandler
+	    public void onBlockBreak(BlockBreakEvent event) {
+	        String player = event.getPlayer().getName();
+	        if (plugin.playersPlaying.contains(player)) {
+	            event.setCancelled(true);
+	        }
+	    }
+
+	    //stop inventorymanagement while ingame
+	    @EventHandler
+	    public void onInventoryClick(InventoryClickEvent event) {
+	        String player = event.getWhoClicked().getName();
+	        if (plugin.playersPlaying.contains(player)) {
+	            event.setCancelled(true);
+	        }
+	    }
 
 }
