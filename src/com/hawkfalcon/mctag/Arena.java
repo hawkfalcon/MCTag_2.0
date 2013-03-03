4118805833ac6a850903749693399c194b513ac1
Doubts <cubixcoders@gmail.com>
2013-03-03 14:49:21 -0800

    Changes in Arena

--- src/com/hawkfalcon/mctag/Arena.java
+++ src/com/hawkfalcon/mctag/Arena.java
@@ -6,17 +6,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.hawkfalcon.mctag.events.TagEvent;
+import com.hawkfalcon.mctag.util.GameState;
 
 public class Arena implements Listener {
 
 	private final MCTag					plugin;
 	private final ConfigurationSection 	config;
 	private final String 				name;
+	private GameState gamestate;
 	
 	public Arena(MCTag plugin, String name, ConfigurationSection sect) {
 		this.plugin = plugin;
 		this.name = name;
 		this.config = sect;
+		
 	}
 	public boolean addPlayer(Player p) {
 		if(plugin.getPlayerMap().containsKey(p.getName())) return false;
@@ -27,6 +30,9 @@ public class Arena implements Listener {
 		if(!plugin.getPlayerMap().containsKey(p.getName())) return false;
 		plugin.getPlayerMap().remove(p.getName());
 		return true;
+	}
+	public GameState getGameState(){
+		return gamestate;
 	}
 	
 	@EventHandler
